34448fd44ccc04815c97ccfa0647ec40fd890956
Mahmoud Elbasuony <MElbasuony@integrant.com>
2018-03-28 20:01:00 +0200

    setup bootstrap 4 & its dependices

--- Server/Integrant.MRR/Integrant.MRR.UI/mrr-react-app/src/App.js
+++ Server/Integrant.MRR/Integrant.MRR.UI/mrr-react-app/src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import "./styles/style.min.css";
 
 class App extends Component {
   render() {
     return (
-      <div >
+      <div className="container">
 
+        <nav className="navbar fixed-top navbar-expand-sm navbar-dark bg-dark">
+          <a className="navbar-brand"   href="javascript:void(0)"><b >MRR</b></a>
+          <ul className="nav navbar-nav navbar-collapse">
+            <li className="nav-item"><a className="nav-link" href="javascript:void(0)">Home</a></li>
+            <li className="nav-item"><a className="nav-link" href="javascript:void(0)">About</a></li>
+          </ul>
+        </nav>
+ 
+        <div className="row app-body">
+          <div className="col">
+            <div className="jumbotron">
+              <h2> Welcome to MRR System</h2>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- Server/Integrant.MRR/Integrant.MRR.UI/mrr-react-app/src/index.js
+++ Server/Integrant.MRR/Integrant.MRR.UI/mrr-react-app/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import "jquery/dist/jquery.min";
+import "popper.js/dist/popper.min";
+import "bootstrap/dist/js/bootstrap.min";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

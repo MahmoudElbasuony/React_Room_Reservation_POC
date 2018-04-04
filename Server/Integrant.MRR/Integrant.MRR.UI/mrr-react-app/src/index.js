133ad92f6c74a7807c5b7247d4eb9314b5fdbd72
Mahmoud Elbasuony <MElbasuony@integrant.com>
2018-04-04 12:47:24 +0200

    testing

--- Server/Integrant.MRR/Integrant.MRR.UI/mrr-react-app/src/index.js
+++ Server/Integrant.MRR/Integrant.MRR.UI/mrr-react-app/src/index.js
@@ -8,8 +8,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 import "sweetalert2/dist/sweetalert2.min.css";
 import "sweetalert2/dist/sweetalert2.all.min";
-import "alertify.js/dist/css/alertify.css";
-import "alertify.js/dist/js/alertify";
 
 import store from "./redux/store";
 import { Provider } from "react-redux";
